ec60ffb94d1944c49a8e71ff36f03c6742ad633d
ushepeleva-creator <u.shepeleva@g.nsu.ru>
2025-11-10 19:39:56 +0300

    ex_1.py

--- ex_1.py
+++ ex_1.py
@@ -2,6 +2,7 @@ rez = []
 for number in range(10):
     rez.append(int(input()))
 new_rez = []
-for el in range(1, 8):
+for el in range(1, 9):
     new_rez.append(rez[el - 1] + rez[el + 1])
 print(new_rez)
+
