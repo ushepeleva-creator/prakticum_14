5932c1cc42d3b94db2553b020da928d056466559
ushepeleva-creator <u.shepeleva@g.nsu.ru>
2025-11-10 19:48:12 +0300

    ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -1,3 +1,7 @@
 list_first = list(map(int, input().split()))
-list_first.remove(3)
-print(list_first)
+
+if list_first.count(3) == 1:
+    list_first.remove(3)
+    print(list_first)
+else:
+    print("in the list more than one 3")
